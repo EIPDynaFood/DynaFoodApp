55be68662e570dd3107ed4c7e633153c7e94ec2f
niffitek <niklas.scheffler@epitech.eu>
2021-12-09 17:49:51 +0100

    Paste example barcode-scanner

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,7 +1,6 @@
-import * as React from 'react';
-
-import { View } from 'react-native';
-import { StyleSheet, Image } from "react-native";
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import {BarCodeScanner} from 'expo-barcode-scanner';
 
 // Currently printing an image
 // How to start:
@@ -11,32 +10,58 @@ import { StyleSheet, Image } from "react-native";
 
 export default function App() {
 
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({type, data}) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={ styles.container }>
-        <Image source={require('./media/food.jpg')} style={{ width: 120, height: 120 }} />
-    </View>
-    )
+      <View style={styles.container}>
+        <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+        />
+        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)}/>}
+      </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#515151',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        elevation: 40,
-        backgroundColor: "#9e9e9e",
-        borderRadius: 45,
-        paddingVertical: 4,
-        paddingHorizontal: 10,
-    },
-    text: {
-        color: 'white',
-        padding: 50,
-        flex: .3,
-        fontWeight: 'bold',
-        fontSize: 20
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    elevation: 40,
+    backgroundColor: "#9e9e9e",
+    borderRadius: 45,
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+  },
+  text: {
+    color: 'white',
+    padding: 50,
+    flex: .3,
+    fontWeight: 'bold',
+    fontSize: 20
+  },
 });
