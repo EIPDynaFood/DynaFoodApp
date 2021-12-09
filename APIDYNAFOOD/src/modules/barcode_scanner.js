2e62adcfdfebb02c028c8fd8d581ab96a7ff7cc4
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2021-12-09 22:47:53 +0100

    fixed route problem

--- APIDYNAFOOD/src/modules/barcode_scanner.js
+++ APIDYNAFOOD/src/modules/barcode_scanner.js
@@ -42,21 +42,23 @@ export const getProduct = async (req, res) => {
             nutriments: [],
             nutrimentsScores: []
         }
-
-        const product = await axios.get(`https://world.openfoodfacts.org/api/v0/product/${req.params.barcode}.json`)
+        const url = `https://world.openfoodfacts.org/api/v0/product/${req.query.barcode}.json`
+        console.log(url)
+        const product = await axios.get('https://world.openfoodfacts.org/api/v0/product/737628064502.json')
         if (typeof product === "undefined" || product == null) {
             res.status(500).send({error: "undefined response from OpenFoodFacts Api"})
         }
-        if (product.status != 1) {
+        if (product.data.status != 1) {
+            console.log(product)
             res.status(204).send({response: "Product not found"})
             return
         }
         if (typeof product === "object") {
-            if (product.product && product.product.ingredients) {
-                response.ingredients = getInnerIngredients(product.product)
+            if (product.data.product && product.data.product.ingredients) {
+                response.ingredients = getInnerIngredients(product.data.product)
             }
         }
-        res.status(200).send(product)
+        res.status(200).send(response)
     } catch(error) {
         console.log(error)
         res.status(500).send("Internal Server Error")
