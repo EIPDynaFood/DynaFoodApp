0ed21a6909e49d1802b5020e046bf538700061a5
bene-labs <62158116+bene-labs@users.noreply.github.com>
2021-12-10 00:16:15 +0100

    fix EndUser Enpoint Exception handling

--- APIDYNAFOOD/src/modules/db/index.js
+++ APIDYNAFOOD/src/modules/db/index.js
@@ -25,9 +25,11 @@ export const getUsers = async (req, res) => {
 export const getUser = async (req, res) => {
     if (req.query.email === undefined && req.query.id === undefined) {
         res.status(400).send({"Error": "No valid search param provided. (Must be either 'email' or 'id'.)"})
+        return
     }
     if (req.query.email !== undefined && req.query.id !== undefined) {
         res.status(400).send({"Error": "You must provide either 'email' or 'id' and cannot search by both."})
+        return
     }
 
     let newUser = (req.query.email !== undefined ?
@@ -41,22 +43,28 @@ export const getUser = async (req, res) => {
     if (req.query.email !== undefined) {
         if (newUser.rows.length == 0) {
             res.status(404).send("There is no EndUser with this email.")
+            return
         }
         res.send(newUser.rows[0]);
+        return
     } else {
         if (newUser.rows.length == 0) {
             res.status(404).send("There is no EndUser with this id.")
+            return
         }
         res.send(newUser.rows[0]);
+        return
     }
 };
 
 export const deleteUser = async (req, res) => {
     if (req.query.email === undefined && req.query.id === undefined) {
         res.status(400).send({"Error": "No valid search param provided. (Must be either 'email' or 'id'.)"})
+        return
     }
     if (req.query.email !== undefined && req.query.id !== undefined) {
         res.status(400).send({"Error": "You must provide either 'email' or 'id' and cannot search by both."})
+        return
     }
 
     let newUser = (req.query.email !== undefined ?
@@ -76,11 +84,14 @@ export const deleteUser = async (req, res) => {
     if (req.query.email !== undefined) {
         if (newUser.rows.length == 0) {
             res.status(200).send("Nothing changed, as there is no EndUser with this email.")
+            return
         }
         res.send({"Deleted": newUser.rows[0]});
+        return
     } else {
         if (newUser.rows.length == 0) {
             res.status(200).send("Nothing changed, as there is no EndUser with this id.")
+            return
         }
         res.send({"Deleted": newUser.rows[0]});
     }
@@ -90,36 +101,44 @@ export const postUser = async (req, res) =>
 {
     if (req.query.firstName === undefined) {
         res.status(400).send({"Error": "Missing required param 'firstName'."})
+        return
     }
     if (req.query.lastName === undefined) {
         res.status(400).send({"Error": "Missing required param 'lastName'."})
+        return
     }
     if (req.query.userName === undefined) {
         res.status(400).send({"Error": "Missing required param 'userName'."})
+        return
     }
     if (req.query.email === undefined) {
         res.status(400).send({"Error": "Missing required param 'email'."})
+        return
     }
     if (req.query.phoneNumber === undefined) {
         res.status(400).send({"Error": "Missing required param 'phoneNumber'."})
+        return
     }
     if (req.query.passcode === undefined) {
         res.status(400).send({"Error": "Missing required param 'passcode'."})
+        return
     }
     if (req.query.emailConfirmed === undefined) {
         res.status(400).send({"Error": "Missing required param 'emailConfirmed'."})
+        return
     }
 
     try {
-        await db_adm_conn.query(`INSERT INTO EndUser (firstName, lastName, userName, email, phoneNumber, passcode, emailConfirmed)
+        let newUser = await db_adm_conn.query(`INSERT INTO EndUser (firstName, lastName, userName, email, phoneNumber, passcode, emailConfirmed)
                                  VALUES ('${req.query.firstName}', '${req.query.lastName}', '${req.query.userName}',
                                      '${req.query.email}', '${req.query.phoneNumber}', '${req.query.passcode}',
-                                     '${req.query.emailConfirmed}');
+                                     '${req.query.emailConfirmed}')
+                                     RETURNING *;
                                  `)
+        res.send(newUser.rows[0]);
+        return
     } catch (error)  {
         res.status(400).send({"Error": "Unable to create new User.", "Details": `${error}`});
+        return
     }
-    let newUser = await db_adm_conn.query(`SELECT * FROM EndUser
-                                      WHERE email='${req.query.email}'`);
-    res.send(newUser.rows[0]);
 };
