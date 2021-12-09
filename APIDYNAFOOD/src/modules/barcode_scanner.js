ebad4be87a3aa37e55783f3055bb0444063a528f
marcel <marcel.taubert@epitech.eu>
2021-12-09 22:56:44 +0100

    Adding first 5 fields of response.

--- APIDYNAFOOD/src/modules/barcode_scanner.js
+++ APIDYNAFOOD/src/modules/barcode_scanner.js
@@ -47,16 +47,25 @@ export const getProduct = async (req, res) => {
         if (typeof product === "undefined" || product == null) {
             res.status(500).send({error: "undefined response from OpenFoodFacts Api"})
         }
-        if (product.status != 1) {
+
+        if (product.data.status != 1) {
             res.status(204).send({response: "Product not found"})
             return
         }
+
+        const data = product["data"]["product"];
+        response.keywords = data["_keywords"];
+        response.allergens = data["allergens"];
+        response.categories = data["categories"].split(',');
+        response.qualities = data["data_quality_tags"];
+        response.warings = data["data_quality_warnings_tags"];
+
         if (typeof product === "object") {
             if (product.product && product.product.ingredients) {
                 response.ingredients = getInnerIngredients(product.product)
             }
         }
-        res.status(200).send(product)
+        res.status(200).send(response).json(); // i think here we need to send the response instead of product, right?
     } catch(error) {
         console.log(error)
         res.status(500).send("Internal Server Error")
