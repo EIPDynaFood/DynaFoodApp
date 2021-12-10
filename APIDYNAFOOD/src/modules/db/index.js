cda4a1cdfeff5e6a820eee35a40a6ffe015327fc
tanyagalkina <tatjana.galkin@epitech.eu>
2021-12-10 13:41:01 +0100

    [ADD] scripts for containers to use DB with npm start

--- APIDYNAFOOD/src/modules/db/index.js
+++ APIDYNAFOOD/src/modules/db/index.js
@@ -1,10 +1,42 @@
 import pg from 'pg'
-const port = 8081
+
+import { DB_STRING } from '../../config/index.js';
+// const port = 8081
 const Client = pg.Client;
+const Pool = pg.Pool;
+
+const connectionString =  'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + DB_STRING
+
+export const poolExample = () => {
+
+    console.log('[EXAMPLE] I am DB Pool example func')
+
+    const pool = new Pool({
+        connectionString,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+    })
+    
+    
+    pool.connect((err, client, release) => {
+        if (err) {
+            return console.error('Error acquiring client', err.stack)
+        }
+        client.query('SELECT NOW()', (err, result) => {
+            release()
+            if (err) {
+            return console.error('Error executing query', err.stack)
+            }
+        console.log(result.rows)
+        })
+    })    
+
+}
 
 export function connect() {
     let db_adm_conn = new Client({
-        connectionString: 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@db/postgres'
+        connectionString
     });
     db_adm_conn.on('error', error => {
         connect();
@@ -19,6 +51,7 @@ export const getEcho = async (req, res) => {
 };
 
 export const getUsers = async (req, res) => {
+    console.log('[LOGGER], getUsers func')
     res.send(await db_adm_conn.query(`SELECT * FROM EndUser`));
 };
 
