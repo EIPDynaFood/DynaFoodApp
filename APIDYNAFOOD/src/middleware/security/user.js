2a9a8194438e45ace0c8245b5b84d394ad31c1e9
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2021-12-15 13:37:16 +0100

    refactoring benedikt + secure + using joi

--- APIDYNAFOOD/src/middleware/security/user.js
+++ APIDYNAFOOD/src/middleware/security/user.js
@@ -1,31 +1,64 @@
 import { db_adm_conn } from "../../modules/db/index.js";
+import _Joi from 'joi';
+import Joi from "joi";
 
-export const checkGetUserReq = (req, res, next) => {
-    if (typeof req.query.email == 'undefined' || req.query.email === null) {
-        res.status(400).send({"Error": "No valid email provided."})
+const schema = _Joi.object({
+    firstName   : _Joi.string()
+                    .pattern(new RegExp('^[a-zA-Z\s\-]'))
+                    .min(3)
+                    .max(20)
+                    .required(),
+    lastName    : _Joi.string()
+                    .pattern(new RegExp('^[a-zA-Z\s\-]'))
+                    .min(3)
+                    .max(20)
+                    .required(),
+    userName    : _Joi.string()
+                    .pattern(new RegExp('^[a-zA-Z0-9]'))
+                    .min(3)
+                    .max(20)
+                    .required(),
+    email       : _Joi.string()
+                    .email(
+                        {
+                            minDomainSegments: 2,
+                            tlds: {
+                                allow: ['com', 'net', 'eu', 'de', 'fr']
+                            }
+                        })
+                    .required(),
+    phoneNumber : _Joi.string()
+                    .pattern(new RegExp('^[0-9\s\+]'))
+                    .min(8)
+                    .max(20)
+                    .required(),
+    passcode    : _Joi.string()
+                    .min(8)
+                    .max(72)
+                    .required()
+})
+
+export const checkUserIdReq = (req, res, next) => {
+    if (typeof req.user.userid == 'undefined' || req.user.userid === null) {
+        res.status(400).send({"Error": "No valid token provided."})
         return
     }
-    /* if (req.query.email !== undefined && req.query.id !== undefined) {
-        res.status(400).send({"Error": "You must provide either 'email' or 'id' and cannot search by both."})
+    next()
+}
+
+export const checkCreateUserReq = (req, res, next) => {
+    const { error, value } = schema.validate(req.body)
+    if (error != undefined) {
+        res.send(400).send({"Error": error})
         return
     }
-
-    let newUser = (req.query.email !== undefined ?
-        await db_adm_conn.query(`SELECT *
-                                          FROM EndUser
-                                          WHERE email = '${req.query.email}';`) :
-        await db_adm_conn.query(`SELECT *
-                                          FROM EndUser
-                                          WHERE enduserid = '${req.query.id}';`))
-
-    if (req.query.email !== undefined) {
-        if (newUser.rows.length == 0) {
-            res.status(404).send("There is no EndUser with this email.")
-            return
-        }
-        res.send(newUser.rows[0]);
+    let prevCheckEmail = await db_adm_conn.query(`
+        SELECT email
+        FROM EndUser
+        WHERE endUserID = ${checkInputBeforeSqlQuery(req.body.email)}`)
+    if (prevCheckEmail.rowCount != 0) {
+        res.status(409).send({"Error": "email already exists"})
         return
     }
-    */
     next()
 }
