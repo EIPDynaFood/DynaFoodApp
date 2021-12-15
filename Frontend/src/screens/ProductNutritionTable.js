f59dd6624c32a0e13950f88a87ddb99ccf465e82
O Elman <oelman@localhost.localdomain>
2021-12-15 23:48:41 +0100

    printing information correctly and inside our app

--- Frontend/src/screens/ProductNutritionTable.js
+++ Frontend/src/screens/ProductNutritionTable.js
@@ -1,4 +1,4 @@
-import {Button, StyleSheet, Text, View} from "react-native";
+import {Button, StyleSheet, Text, View, ScrollView} from "react-native";
 import React, {useState, useEffect} from "react";
 import {FAB} from "react-native-elements";
 import {RequireJwt} from "../components/RequireJwt";
@@ -8,15 +8,28 @@ const axios = require('axios');
 export default function ProductNutritionTable({navigation, route}) {
   const {itemId, productData} = route.params;
 
+  // Printing to check what is inside of productdata
+  for (let key in productData["nutriments_g_pro_100g"]) {
+    console.log(key, productData["nutriments_g_pro_100g"][key])
+  }
+
+  // Function that turn Object into Array
+  // Use map to iterate over Array and return it as Text
+  function display() {
+    let arr = Object.entries(productData["nutriments_g_pro_100g"])
+    return arr.map((item) => {
+      return(
+        <Text>
+          {item}
+        </Text>
+      );
+    });
+  }
+
   return (
       <RequireJwt>
         <View>
-          <Text>Product</Text>
-          {productData === null ? (<FAB color="grey" size="small" loading/>) : (
-              <View>
-                <Text style={{fontSize: 50}}>{productData["name"]}</Text>
-              </View>
-          )}
+          {display()}
         </View>
       </RequireJwt>
   );
