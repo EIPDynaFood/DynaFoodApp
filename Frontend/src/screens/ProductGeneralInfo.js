b1b9488d0449feb806935b08799eba53e045e602
niffitek <niklas.scheffler@epitech.eu>
2021-12-16 03:08:21 +0100

    adjust image

--- Frontend/src/screens/ProductGeneralInfo.js
+++ Frontend/src/screens/ProductGeneralInfo.js
@@ -2,32 +2,67 @@ import {Button, StyleSheet, Text, View, Image, FlatList, ScrollView} from "react
 import React, {useState, useEffect} from "react";
 import {FAB, ListItem} from "react-native-elements";
 import {RequireJwt} from "../components/RequireJwt";
+import {LinearGradient} from "expo-linear-gradient";
 
 const axios = require('axios');
 
 export default function ProductGeneralInfo({navigation, route}) {
   const {itemId, productData} = route.params;
 
+  let ingredients = ""
+  productData['ingredients']['ingredients'].map((item, index) => {
+    if (index === 0)
+      ingredients += item['name']
+    else
+      ingredients += ", " + item['name']
+  })
+
+  console.log(productData)
+
   return (
       <RequireJwt>
-        <View style={{flexDirection: "row"}}>
-          <Text style={{fontSize: 50}}>{productData["name"]}</Text>
-          <Image source={{uri: "https://images.openfoodfacts.org/images/products/506/033/563/5266/front_de.14.200.jpg"}}
-                 style={{resizeMode: "contain", width: 100, height: 100}}/>
+        <View style={{flex: 1}}>
+          <Image source={{uri: productData['images']}}
+                 style={style.imageStyle}/>
+          <LinearGradient style={style.gradientStyle}
+                          colors={['rgba(0,0,0,0.5)', 'transparent']}
+                          start={{x: 0, y: 1.0}}
+                          end={{x: 0, y: 0}}/>
+          <Text style={style.headlineStyle}>{productData["name"]}</Text>
         </View>
-        <ScrollView>
-            {
-              productData['ingredients']['ingredients'].map((item, index) => (
-                  <ListItem key={index}>
-                    <ListItem.Content>
-                      <Text>{item["name"]}</Text>
-                    </ListItem.Content>
-                  </ListItem>
-              ))
-            }
-          </ScrollView>
+        <View>
+          <Text style={style.ingredientStyle}>{ingredients}</Text>
+        </View>
       </RequireJwt>
   );
 }
 
+const style = StyleSheet.create({
+  gradientStyle: {
+    //flex: 1,
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: 0,
+    width: "100%",
+    height: 150,
+  },
+  imageStyle: {
+    flex: 1,
+    position: "absolute",
+    top: 0,
+    resizeMode: "cover",
+    width: "100%",
+    height: 150,
+  },
+  headlineStyle: {
+    position: "absolute",
+    top: 90,
+    flex: 1,
+    fontSize: 50,
+    color: "rgba(255,255,255,1)"
+  },
+  ingredientStyle: {
+    //flex: 1,
+  }
+})
 
