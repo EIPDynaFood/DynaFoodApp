1e682ba8c58361f37af47c0bcee65164c3246c45
Skidbladnir <n.jurdyc@gmail.com>
2021-12-17 04:04:12 +0100

    login + register screnn clean, working

--- Frontend/src/screens/Login.js
+++ Frontend/src/screens/Login.js
@@ -1,67 +1,127 @@
-import {Button, StyleSheet, Text, View, SafeAreaView, TextInput} from "react-native";
+import {StyleSheet, View, TextInput, Image } from "react-native";
 import React, { useState, useEffect } from "react";
-import { FAB } from 'react-native-elements';
-import { Icon } from 'react-native-elements'
+import { FAB, Button} from 'react-native-elements';
 import {useNavigation} from "@react-navigation/native";
 import useJwt from "../../Jwt"
 import axios from "axios";
 
 
-const styles = StyleSheet.create({
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
-});
 
 export default function Login({navigation, route}) {
     const {login} = useJwt()
 
-    const guestEmail = "twasdawadswdwadwdasaswadwadsest@test.com"
+    const guestEmail = "i@i.com"
     const guestPassword = "password"
 
     const [email, onChangeEmail] = React.useState("");
     const [password, onChangePassword] = React.useState("");
 
+    const sendLogin = () => {
+        var config = {
+            method: 'get',
+            url: 'https://dynafood.herokuapp.com/login?email=' + `${email}` + '&password=' + `${password}`,
+        };
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                navigation.navigate("History")
+            })
+            .catch(function (error) {
+                alert("Wrong user credentials:\n" + error.message)
+                console.log(error);
+            });
+    };
+
     return (
-        <View>
+        <View style={styles.container}>
+
+            <Image source={require('../../assets/logo_frame.png')}
+                    style={{width:270, height:150, marginBottom:50}}/>
             <TextInput
+                placeholder="Email..."
                 style={styles.input}
                 onChangeText={onChangeEmail}
-                value={email}
-            />
+                value={email}/>
             <TextInput
+                placeholder="Password..."
                 style={styles.input}
                 onChangeText={onChangePassword}
                 value={password}
                 secureTextEntry={true}
             />
-            <Text>Login</Text>
-            <Button title="register" onPress={() => navigation.navigate("Register")}/>
-            <Button title="test" onPress={() =>
-                console.log("email = " + email + "\tpassword= " + password)
-            }/>
-            <Button style={{top: 10}} title="simulate fake jwt" onPress={() => {
-              login("thisWountAuthForReal")
-              navigation.navigate("History")
-            }}/>
-            <Button style={{top: 10}} title="Login" onPress={() => {
-                var config = {
-                    method: 'get',
-                    url: 'https://dynafood.herokuapp.com/login?email=' + `${email}` + '&password=' + `${password}`,
-                };
-                axios(config)
-                    .then(function (response) {
-                        console.log(JSON.stringify(response.data));
-                        navigation.navigate("History")
-                    })
-                    .catch(function (error) {
-                        alert("Wrong user credentials:\n" + error.message)
-                        console.log(error);
-                    });
-            }}/>
+
+            <Button
+                title="Login"
+                containerStyle= {{
+                    margin:15
+                }}
+                buttonStyle={{
+                    width:"40%",
+                    borderRadius:5,
+                    backgroundColor:"#2E4D44",
+                }}
+                titleStyle={{color:"white", flex:1}}
+                onPress={() => {
+                    sendLogin();
+                }}
+                />
+
+            <Button
+                title="Register"
+                containerStyle= {{
+                    margin:15
+                }}
+                buttonStyle={{
+                    width:"40%",
+                    borderRadius:5,
+                    backgroundColor:"#FFF",
+                }}
+                titleStyle={{color:"#2E4D44", flex:1}}
+                onPress={() => {
+                    navigation.navigate("Register")
+                }}
+                />
+
+
+            {/* <Button title="Login" buttonStyle={{marginBottom:20, backgroundColor:"#2E4D44", width:120}}/>
+            <Button title="register" onPress={() => navigation.navigate("Register")}/> */}
+
+            <FAB
+              color="#2E4D44"
+              title="Login as guest"
+              style={{position: "absolute", bottom: 16, right: 16}}
+              onPress={() => {
+                  onChangeEmail(guestEmail);
+                  onChangePassword(guestPassword);
+                }}
+                />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        width: "70%",
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: "lightgrey",
+        borderRadius: 5,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    loginbtn: {
+        width: "50%",
+        backgroundColor: "#2E4D44",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        marginBottom: 10,
+    },
+});
--- Frontend/src/screens/Register.js
+++ Frontend/src/screens/Register.js
@@ -1,7 +1,6 @@
-import {Button, StyleSheet, Text, View} from "react-native";
+import {StyleSheet, View, Image, TextInput} from "react-native";
 import React, { useState, useEffect } from "react";
-import { FAB } from 'react-native-elements';
-import { Icon } from 'react-native-elements'
+import { Button } from 'react-native-elements';
 import {useNavigation} from "@react-navigation/native";
 import useJwt from "../../Jwt"
 import axios from "axios";
@@ -13,45 +12,124 @@ export default function Register() {
     const firstName = "firstName"
     const lastName = "lastName"
     const userName = "userName"
-    const email = "i@i.com"
     const phoneNumber = "00000000"
-    const password = "password"
+
+    const [email, onChangeEmail] = React.useState("");
+    const [password, onChangePassword] = React.useState("");
+    const [ConPassword, onChangeConPassword] = React.useState("")
+
+    const sendRegister = () => {
+        var qs = require('qs');
+            var data = qs.stringify({
+                'firstName': `${firstName}`,
+                'lastName': `${lastName}`,
+                'userName': `${userName}`,
+                'email': `${email}`,
+                'phoneNumber': `${phoneNumber}`,
+                'password': `${password}`
+            });
+            var config = {
+                method: 'post',
+                url: 'https://dynafood.herokuapp.com/signup',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                data : data
+            };
+            axios(config)
+                .then(function (response) {
+                    console.log(JSON.stringify(response.data));
+                    login(JSON.stringify(response.data))
+                    navigation.navigate("History");
+                })
+                .catch(function (error) {
+                    alert("Something went wrong in your Registration process:\n" + error.message)
+                    console.log(error);
+                    console.log(error.response);
+                    console.log("email in use");
+                });
+    }
 
     return (
-        <View style={StyleSheet.absoluteFillObject}>
-            <Text>Register</Text>
-            <Button style={{top: 10}} title="Login" onPress={() => {
-                var qs = require('qs');
-                var data = qs.stringify({
-                    'firstName': `${firstName}`,
-                    'lastName': `${lastName}`,
-                    'userName': `${userName}`,
-                    'email': `${email}`,
-                    'phoneNumber': `${phoneNumber}`,
-                    'password': `${password}`
-                });
-                var config = {
-                    method: 'post',
-                    url: 'https://dynafood.herokuapp.com/signup',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    data : data
-                };
-                axios(config)
-                    .then(function (response) {
-                        console.log(JSON.stringify(response.data));
-                        login(JSON.stringify(response.data))
-                        navigation.navigate("History");
-                    })
-                    .catch(function (error) {
-                        alert("Something went wrong in your Registration process:\n" + error.message)
-                        console.log(error);
-                        console.log(error.response);
-                        console.log("email in use");
-                    });
-            }}/>
-
+        <View style={styles.container}>
+            <Image source={require('../../assets/logo_frame.png')}
+                    style={{width:270, height:150, marginBottom:50}}/>
+            <TextInput
+                placeholder="Email..."
+                style={styles.input}
+                onChangeText={onChangeEmail}
+                value={email}/>
+            <TextInput
+                placeholder="Password..."
+                style={styles.input}
+                onChangeText={onChangePassword}
+                value={password}
+                secureTextEntry={true}
+            />
+            <TextInput
+                placeholder="Confirme your Password..."
+                style={styles.input}
+                onChangeText={onChangeConPassword}
+                value={ConPassword}
+                secureTextEntry={true}
+            />
+            <Button
+                title="Register"
+                containerStyle= {{
+                    margin:15
+                }}
+                buttonStyle={{
+                    width:"40%",
+                    borderRadius:5,
+                    backgroundColor:"#2E4D44",
+                }}
+                titleStyle={{color:"white", flex:1}}
+                onPress={() => {
+                    sendRegister();
+                }}
+                />
+            <Button
+                title="Cancel"
+                containerStyle= {{
+                    margin:15
+                }}
+                buttonStyle={{
+                    width:"40%",
+                    borderRadius:5,
+                    backgroundColor:"#FFF",
+                }}
+                titleStyle={{color:"#2E4D44", flex:1}}
+                onPress={() => {
+                    navigation.navigate("Login")
+                }}
+                />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    input: {
+        width: "70%",
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderColor: "lightgrey",
+        borderRadius: 5,
+    },
+    loginbtn: {
+        width: "50%",
+        backgroundColor: "#2E4D44",
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        marginBottom: 10,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
