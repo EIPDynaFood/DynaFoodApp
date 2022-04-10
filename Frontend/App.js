fbcdbeac151f8a2bc0ffa3fd72ba0d11aea8575f
niffitek <niklas.scheffler@epitech.eu>
2022-04-10 14:54:43 +0200

    remove rootNavigation

--- Frontend/App.js
+++ Frontend/App.js
@@ -12,7 +12,6 @@ import Settings from "./src/screens/Settings";
 import {Icon} from "react-native-elements";
 import useJwt, {JwtProvider} from "./Jwt"
 import {RequireJwt} from "./src/components/RequireJwt"
-import { navigationRef } from './RootNavigation';
 import isReadyRef from "react-native/Libraries/Components/DrawerAndroid/DrawerLayoutAndroid";
 
 const Stack = createNativeStackNavigator();
@@ -22,9 +21,7 @@ export function Navigation() {
 
   return (
       jwt === null ? (
-          <NavigationContainer ref={navigationRef} onReady={() => {
-              isReadyRef.current = true;
-          }}>
+          <NavigationContainer>
             <Stack.Navigator screenOptions={{
               headerStyle: {
                 backgroundColor: '#376D55',
@@ -38,9 +35,7 @@ export function Navigation() {
               <Stack.Screen name="Register" component={Register}/>
             </Stack.Navigator>
           </NavigationContainer>) : (
-          <NavigationContainer ref={navigationRef} onReady={() => {
-              isReadyRef.current = true;
-          }}>
+          <NavigationContainer>
             <Stack.Navigator screenOptions={{
               headerStyle: {
                 backgroundColor: '#376D55',
