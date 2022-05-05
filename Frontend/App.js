0bc45fa8235ba8c72b6ca7d6e3422b97dd996a50
Niffi <niklas.scheffler@epitech.eu>
2022-05-05 12:49:29 +0200

    start new test feature

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+//test feature
 import {Text, View, StyleSheet, Button, LogBox, StatusBar, StatusBarStyle} from 'react-native';
 import Scanner from "./src/screens/Scanner";
 import Product from "./src/screens/Product";
