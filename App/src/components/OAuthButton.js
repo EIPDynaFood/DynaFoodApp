cdbd1d3c218eda418268bcd700ad574513c23cd0
niffitek <nikschef01@gmail.com>
2023-01-08 08:49:29 +0100

    Google OAuth (untested!!!)

--- App/src/components/OAuthButton.js
+++ App/src/components/OAuthButton.js
@@ -1,9 +1,25 @@
 import {TouchableHighlight, View} from "react-native";
 import {SocialIcon} from "react-native-elements";
+import axios from "axios";
+import translations from "../../translations/components/ProductHistory.json";
+import useJwt from "../../Jwt";
 
 export function OAuthButton(props) {
+    const {login} = useJwt()
+
     const signIn = () => {
         console.log(props.service)
+        if (props.service === "Google") {
+            axios.get('http://x2024dynafood545437452001.westeurope.cloudapp.azure.com:8081/user/login/google').then((res) => {
+                console.log(res.data)
+                login(res.data);
+                props.navigation.navigate("History")
+            }).catch((err) => {
+                console.log('catch');
+                alert(err.message);
+                console.log(err);
+            });
+        }
     }
 
     return (
