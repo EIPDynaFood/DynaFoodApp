3da975768e8b4bf9ab27fd8459138eaef4808e90
niffitek <nikschef01@gmail.com>
2023-04-12 16:02:14 +0200

    test commit

--- App/src/components/ProductHistory.js
+++ App/src/components/ProductHistory.js
@@ -9,6 +9,8 @@ import { styles } from "../styles/Style";
 import useLang from "../../Language"
 import { endpoint } from '../../config';
 
+// this is a test
+
 export default function ProductHistory(props) {
   const [historyData, setHistoryData] = useState(props.data);
   const navigation = useNavigation();
