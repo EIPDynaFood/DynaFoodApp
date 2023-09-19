1171eaf48a979988c8aafb460b701c919d389c48
niffitek <nikschef01@gmail.com>
2023-09-19 11:41:58 +0200

    translation in EN and DE for Headers

--- App/App.js
+++ App/App.js
@@ -9,12 +9,11 @@ import History from "./src/screens/History";
 import Login from "./src/screens/Login";
 import Register from "./src/screens/Register";
 import Settings from "./src/screens/Settings";
-import SearchResult from './src/screens/SearchResult';
 import MissingProduct from "./src/screens/MissingProduct";
 import Feedback from "./src/screens/Feedback";
 import Swiper from './src/screens/Swiper';
 import {Icon} from "react-native-elements";
-import {LangProvider} from "./Language";
+import useLang, {LangProvider} from "./Language";
 import SendEmail from "./src/screens/SendEmail";
 import VerifyCode from "./src/screens/Authentication";
 import ResetPassword from "./src/screens/ResetPassword";
@@ -22,10 +21,14 @@ import ShoppingOverview from "./src/screens/ShoppingOverview";
 import ShoppingListItems from "./src/screens/ShoppingListItems";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as SecureStore from "expo-secure-store";
+import translations from "./translations/App.json";
 
 const Stack = createNativeStackNavigator();
 export function Navigation(props) {
   let swiper = localStorage.getItem('Swiper');
+
+    const translations = require("./translations/App.json")
+    const {lang} = useLang()
 
   return (
       <GestureHandlerRootView style={{flex: 1}}>
@@ -40,16 +43,16 @@ export function Navigation(props) {
               }
             }}>
                 {swiper === null ?
-                    <Stack.Screen options={{headerShown: false}} name="Swiper" component={Swiper}/>
+                    <Stack.Screen options={{headerShown: false, title: translations["Welcome"][lang]}} name="Swiper" component={Swiper}/>
                     : <></>}
-                <Stack.Screen options={{headerShown: false}} name="Login" component={Login}/>
-                <Stack.Screen options={{headerShown: false}} name="Register" component={Register}/>
-                <Stack.Screen options={{title: "Reset Password"}} name="SendEmail" component={SendEmail}/>
-                <Stack.Screen options={{title: "Reset Password"}} name="VerifyCode" component={VerifyCode}/>
-                <Stack.Screen options={{title: "Reset Password"}} name="ResetPassword" component={ResetPassword}/>
+                <Stack.Screen options={{headerShown: false, title: translations["Login"][lang]}} name="Login" component={Login}/>
+                <Stack.Screen options={{headerShown: false, title: translations["Register"][lang]}} name="Register" component={Register}/>
+                <Stack.Screen options={{title: translations["Reset Password"][lang]}} name="SendEmail" component={SendEmail}/>
+                <Stack.Screen options={{title: translations["Reset Password"][lang]}} name="VerifyCode" component={VerifyCode}/>
+                <Stack.Screen options={{title: translations["Reset Password"][lang]}} name="ResetPassword" component={ResetPassword}/>
 
                 <Stack.Screen name="History" component={History}
-                            options={({navigation}) => ({
+                            options={({navigation}) => ({title: "DynaFood",
                               headerRight: () => (
                                   <View style={{display:"flex", flexDirection:"row"}}>
                                       <View style={{right: 15}}>
@@ -70,14 +73,13 @@ export function Navigation(props) {
                               )
                             })}
               />
-              <Stack.Screen name="Scanner" component={Scanner}/>
-              <Stack.Screen name="Feedback" component={Feedback}/>
-              <Stack.Screen name="Product" component={Product}/>
-              <Stack.Screen name="Settings" component={Settings}/>
-              <Stack.Screen name="Search Result" component={SearchResult}/>
-              <Stack.Screen name="MissingProduct" component={MissingProduct}/>
-              <Stack.Screen name="ShoppingOverview" component={ShoppingOverview}/>
-                <Stack.Screen name="ShoppingListItems" component={ShoppingListItems}/>
+              <Stack.Screen options={{title: translations["Scanner"][lang]}} name="Scanner" component={Scanner}/>
+              <Stack.Screen options={{title: translations["Feedback"][lang]}} name="Feedback" component={Feedback}/>
+              <Stack.Screen options={{title: translations["Product"][lang]}} name="Product" component={Product}/>
+              <Stack.Screen options={{title: translations["Settings"][lang]}} name="Settings" component={Settings}/>
+              <Stack.Screen options={{title: translations["Missing Product"][lang]}} name="MissingProduct" component={MissingProduct}/>
+              <Stack.Screen options={{title: translations["Shopping Lists"][lang]}} name="ShoppingOverview" component={ShoppingOverview}/>
+              <Stack.Screen options={{title: translations["Shopping Items"][lang]}} name="ShoppingListItems" component={ShoppingListItems}/>
             </Stack.Navigator>
           </NavigationContainer>
       </GestureHandlerRootView>)
